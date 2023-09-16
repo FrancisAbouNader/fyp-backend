b54c976633be578e37ba2d4ccd13cfaded5fb07d
gaoun <georgesaoun42@gmail.com>
2023-09-16 12:21:27 +0300

    .

--- app/Validations/ItemValidation.php
+++ app/Validations/ItemValidation.php
@@ -28,7 +28,7 @@ class ItemValidation
     {
 
         return Validator::make(request()->all(), [
-            "product_id" => "required|integer|unique:products,id,deleted_at,NULL",
+            "product_id" => "required|integer|exits:products,id",
             'imei' => "required|string",
             'name' => "required|string",
             'company_id' => "required|integer|exists:companies,id"
